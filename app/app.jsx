aab0a550ae9742bdfb389564f2b7b518f945950d
Francocotter <chockyeizo@hotmail.com>
2017-02-05 23:06:05 +0800

    update

--- app/app.jsx
+++ app/app.jsx
@@ -5,6 +5,12 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Example = require('Example');
+
+
+//Load foundation
+require('style!css!foundation-sites/dist/foundation.min.css');
+
+$(document).foundation();
 ReactDOM.render(
     <Router history={hashHistory}>
       <Route path='/' component={Main}>
